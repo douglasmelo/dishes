ee7a40d4f53c0f6b27faa24c1c3c6ed3498b9560
Douglas Melo <douglas.melo@brasilct.com>
2018-03-25 18:32:44 -0300

    Fix rollbackFor properties and inject customer and product repository

--- src/main/java/com/vanhack/dishes/service/OrderServiceImpl.java
+++ src/main/java/com/vanhack/dishes/service/OrderServiceImpl.java
@@ -10,7 +10,9 @@ import com.vanhack.dishes.builder.OrderBuilder;
 import com.vanhack.dishes.model.Order;
 import com.vanhack.dishes.model.OrderStatus;
 import com.vanhack.dishes.model.request.OrderRequest;
+import com.vanhack.dishes.repository.CustomerRepository;
 import com.vanhack.dishes.repository.OrderRepository;
+import com.vanhack.dishes.repository.ProductRepository;
 
 @Service
 public class OrderServiceImpl implements OrderService{
@@ -18,22 +20,24 @@ public class OrderServiceImpl implements OrderService{
 	private static final Logger LOGGER = LoggerFactory.getLogger(OrderServiceImpl.class);
 
 	@Autowired OrderRepository orderRepository;
+	@Autowired CustomerRepository customerRepository;
+	@Autowired ProductRepository productRepository;
 
 	@Override
-	@Transactional(noRollbackFor = Throwable.class)
+	@Transactional(rollbackFor = Throwable.class)
 	public void save(OrderRequest request) {
 
 		LOGGER.info("Begin method save for order {}", request);
 
 		//TODO: Send to RabbitMQ
-		Order order = OrderBuilder.New(request).build();
+		Order order = OrderBuilder.New(request, customerRepository, productRepository).build();
 		Order persistedOrder = orderRepository.save(order);
 
 		LOGGER.info("Finish method save for order {}", persistedOrder);
 	}
 
 	@Override
-	@Transactional(noRollbackFor = Throwable.class)
+	@Transactional(rollbackFor = Throwable.class)
 	public void cancel(Long orderId) {
 		LOGGER.info("Begin method cancel for order {}", orderId);
 		
