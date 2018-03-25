b27aefd86761bd1580b8dbbd7ccb91ef25d88904
Douglas Melo <douglas.melo@brasilct.com>
2018-03-25 20:07:12 -0300

    Include order dependencies

--- src/main/java/com/vanhack/dishes/builder/OrderBuilder.java
+++ src/main/java/com/vanhack/dishes/builder/OrderBuilder.java
@@ -1,33 +1,75 @@
 package com.vanhack.dishes.builder;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import com.vanhack.dishes.model.Customer;
 import com.vanhack.dishes.model.Order;
+import com.vanhack.dishes.model.OrderItem;
 import com.vanhack.dishes.model.OrderStatus;
+import com.vanhack.dishes.model.Product;
+import com.vanhack.dishes.model.request.OrderItemRequest;
 import com.vanhack.dishes.model.request.OrderRequest;
+import com.vanhack.dishes.repository.CustomerRepository;
+import com.vanhack.dishes.repository.ProductRepository;
 
 public class OrderBuilder {
   
+	private CustomerRepository customerRepository;
+	private ProductRepository productRepository;
 	private OrderRequest orderRequest;
 	
-	private OrderBuilder(OrderRequest orderRequest) {
+	private OrderBuilder(OrderRequest orderRequest, CustomerRepository customerRepository, ProductRepository productRepository) {
+		this.productRepository = productRepository;
+		this.customerRepository = customerRepository;
 		this.orderRequest = orderRequest;
 	}
 	
-	public static OrderBuilder New(OrderRequest orderRequest) {
-		return new OrderBuilder(orderRequest);
+	public static OrderBuilder New(OrderRequest orderRequest, CustomerRepository customerRepository, ProductRepository productRepository) {
+		return new OrderBuilder(orderRequest, customerRepository, productRepository);
 	}
 	
 	public Order build() {
 		Order order = new Order();
 		
 		order.setContact(orderRequest.getContact());
-//		order.setCustomer(orderRequest.getCustomerUuid());
+		order.setCustomer(getCustomer());
 		order.setDeliveryAddress(orderRequest.getDeliveryAddress());
-//		order.setItems(orderRequest.getOrderItems());
 		order.setStoreId(orderRequest.getStoreId());
 		order.setTotal(orderRequest.getTotal());
 		order.setStatus(OrderStatus.PENDING);
+		order.setItems(buildOrderItems(orderRequest.getOrderItems(), order));
 		
 		return order;
+	}
+
+	private Collection<OrderItem> buildOrderItems(List<OrderItemRequest> orderItems, Order order) {
+		return Optional.ofNullable(orderItems)
+			.map(items -> items.stream()
+					.map(itemRequest -> {
+						OrderItem item = new OrderItem();
+						item.setProduct(buildProduct(itemRequest.getProductId()));
+						item.setPrice(itemRequest.getPrice());
+						item.setQuantity(itemRequest.getQuantity());
+						//TODO: Total could be calculated on backend
+//						item.setTotal(itemRequest.getPrice().multiply(new BigDecimal(itemRequest.getQuantity())));
+						item.setTotal(itemRequest.getTotal());
+						item.setOrder(order);
+						
+						return item;
+					}).collect(Collectors.toList()))
+			.orElse(new ArrayList<>(0));
+	}
+
+	private Product buildProduct(String productId) {
+		return productRepository.findByUuid(productId);
+	}
+
+	private Customer getCustomer() {
+		return customerRepository.findByUuid(orderRequest.getCustomerId());
 	}
 	
 }
