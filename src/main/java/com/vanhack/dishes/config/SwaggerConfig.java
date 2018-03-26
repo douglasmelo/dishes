c52343f97d2f32acbb2db27e5de230caf95c98a1
Douglas Melo <douglas.melo@brasilct.com>
2018-03-26 08:09:29 -0300

    Configure swagger

---
+++ src/main/java/com/vanhack/dishes/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.vanhack.dishes.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+
+	@Bean
+	public Docket apiOrder(){
+	    return new Docket(DocumentationType.SWAGGER_2)
+	    	.groupName("dishes")	
+	        .select()
+	        .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+	        .paths(PathSelectors.regex("/api/v1/*([^/]*).+"))
+	        .build()
+	        .apiInfo(apiInfo());
+	}
+	
+	private ApiInfo apiInfo() {
+	    return new ApiInfoBuilder()
+	        .title("Skip The Dishes")
+	        .build();
+	}
+	
+}
